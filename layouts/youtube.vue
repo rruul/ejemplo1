a9f992029de26c7eb6e12a1792a0ba4b01c8e0ee
Rruul <ortega.zavala.raul@gmail.com>
2024-05-20 15:30:49 -0600

    Proyecto terminado, abierto a posibles modificaciones

--- layouts/youtube.vue
+++ layouts/youtube.vue
@@ -1,141 +1,149 @@
 <template>
-    <v-app id="inspire">
-      <v-system-bar>
-        <v-spacer />
-        <v-icon>mdi-square</v-icon>
-        <v-icon>mdi-circle</v-icon>
-        <v-icon>mdi-triangle</v-icon>
-      </v-system-bar>
-      
-  
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer />
+      <v-icon>mdi-square</v-icon>
+      <v-icon>mdi-circle</v-icon>
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+    <ui-alert
+      v-if="showAlert"
+      :type="type"
+      :color="color"
+      :border="border"
+      :alert-text="text"
+      :icon="icon"
+    />
+    <v-app-bar
+      app
+      clipped-right
+      flat
+      height="72"
+    >
+      <v-spacer />
+      <v-responsive max-width="156">
+        <v-text-field
+          dense
+          flat
+          hide-controls
+          rounded
+          solo-inverted
+        />
+      </v-responsive>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      width="300"
+    >
       <v-navigation-drawer
-        color="grey-lighten-3"
-        rail
+        v-model="drawer"
+        absolute
+        color="grey lighten-3"
+        mini-variant
       >
         <v-avatar
           class="d-block text-center mx-auto mt-4"
           color="grey-darken-1"
           size="36"
-        ></v-avatar>
-  
-        <v-divider class="mx-3 my-5"></v-divider>
-  
+        />
+        <v-divider class="mx-3 mv-5" />
         <v-avatar
           v-for="n in 6"
           :key="n"
           class="d-block text-center mx-auto mb-9"
           color="grey-lighten-1"
           size="28"
-        ></v-avatar>
-      </v-navigation-drawer>
-  
-      <v-navigation-drawer width="244">
-        <v-sheet
-          color="grey-lighten-5"
-          height="128"
-          width="100%"
-        ></v-sheet>
-  
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            :title="`Item ${ n }`"
-            link
-          ></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-  
-      <v-app-bar
-        class="px-3"
-        color="grey-lighten-4"
-        flat
-        height="72"
-      >
-        <v-spacer></v-spacer>
-  
-        <v-responsive max-width="156">
-          <v-text-field
-            bg-color="grey-lighten-1"
-            density="compact"
-            flat
-            hide-details
-            rounded="pill"
-            variant="solo-filled"
-          ></v-text-field>
-        </v-responsive>
-      </v-app-bar>
-  
-      <v-main>
-        <ui-alert
-        v-if="showAlert"
-        :type="success"
-        :color="green darkeen-4"
-        :border="top"
-        :alert-text="Alerta Inicial"
-        :icon="mdi-account"
         />
-        <Nuxt />
-      </v-main>
-  
-      <v-navigation-drawer location="right">
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            :title="`Item ${ n }`"
-            link
-          ></v-list-item>
-        </v-list>
       </v-navigation-drawer>
-  
-      <v-footer
-        app
-        height="72"
+      <v-sheet
+        color="grey-lighten-5"
+        height="128"
+        width="100%"
+      />
+      <v-list
+        class="pl-14"
+        shaped
       >
-        <v-text-field
-          bg-color="grey-lighten-1"
-          class="overflow-hidden"
-          density="compact"
-          flat
-          hide-details
-          rounded="pill"
-          variant="solo-filled"
-        ></v-text-field>
-      </v-footer>
-    </v-app>
-  </template>
+        <v-list-item
+          v-for="n in 5"
+          :key="n"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-navigation-drawer
+      app
+      cliped
+      right
+    >
+      <v-list>
+        <v-list-item
+          v-for="n in 5"
+          :key="n"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <Nuxt />
+    </v-main>
+    <v-footer
+      app
+      color="trasnparent"
+      height="72"
+      inset
+    >
+      <v-text-field
+        bg-color="grey-lighten-1"
+        dense
+        flat
+        hide-details
+        rounded
+        solo
+      />
+    </v-footer>
+  </v-app>
+</template>
 
- <script>
-  export default {
-    data () {
-      return {
-        drawer: null,
-        showAlert: false,
-        type: '',
-        color: '',
-        border: '',
-        text: '',
-        icon: ''
-      }
-    }, 
-    created () {
-      this.$nuxt.$on('show-alert', $event => this.cambiaDatos($event))
-    },
-    methods: {
-      cambiaDatos (datos) {
-        console.log('@@ ->', datos)
-        this.color = datos.coleor
-        this.type = datos.type
-        this.text = datos.text
-        this.border = datos.border
-        this.width = datos.width
-        this.icon = datos.icon
-        this.showAlert = true
-        setTimeout(()=>{
-          this.showAlert = false
-        }, datos.time)
-      }
+<script>
+export default {
+  data () {
+    return {
+      drawer: null,
+      showAlert: false,
+      type: '',
+      color: '',
+      border: '',
+      text: '',
+      icon: ''
+    }
+  },
+  created () {
+    this.$nuxt.$on('show-alert', $event => this.cambiaDatos($event))
+  },
+  methods: {
+    cambiaDatos (datos) {
+      console.log('@@ ->', datos)
+      this.color = datos.color
+      this.type = datos.type
+      this.text = datos.text
+      this.border = datos.border
+      this.width = datos.width
+      this.icon = datos.icon
+      this.showAlert = true
+      setTimeout(() => {
+        this.showAlert = false
+      }, datos.time)
     }
   }
+}
 </script>
